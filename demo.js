f7add9548af68b15fcfc194e557eefbb3c27b7e5
whiteand <andrewbeletskiy@gmail.com>
2021-10-24 11:57:13 +0300

    removed demo

--- demo.js
+++
@@ -1,9 +0,0 @@
-// I have array of values
-
-const enumValues = ["Read", "Eval", "Print"];
-
-// I want to generate some code
-for (let i = 0; i < enumValues.length; i++) {
-  const enumValue = enumValues[i];
-  writeline(`const ${enumValue.toUpperCase()} = ${i};`);
-}
